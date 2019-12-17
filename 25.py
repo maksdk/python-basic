2f1f701044787751a6a8e327aa44744d9d349f22
maksdk <havryshchenko@gmail.com>
2019-12-17 12:12:35 +0200

    magic-numbers

---
+++ 25.py
@@ -0,0 +1,8 @@
+king = 'King Balon the 6th'
+
+# BEGIN
+floors_count = 6
+rooms_count_on_floor = 17
+
+print(king + " " + "has" + " " + str(floors_count * rooms_count_on_floor) + " " + "rooms.")
+# END
