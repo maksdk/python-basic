7c9ed1cf51a1a8df49a81d0d0843940e5f998634
maksdk <havryshchenko@gmail.com>
2019-12-17 09:54:02 +0200

    naming

---
+++ 21.py
@@ -0,0 +1,5 @@
+first_number = 20
+second_number = -100
+print(first_number * second_number)
+
+# В Python переменые именуются в стиле snake_case: слова записываються строчными буквами и разделяються нижним подчеркиванием
