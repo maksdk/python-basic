31b416dc67487ca5b14adbbaa5568ead075044f5
maksdk <havryshchenko@gmail.com>
2019-12-19 10:28:39 +0200

    function expresition

---
+++ 34.py
@@ -0,0 +1,8 @@
+from hexlet.code_basics import calculate_distance
+
+# BEGIN
+dist1 = calculate_distance("Winterfell", "The Twins")
+dist2 = calculate_distance("The Twins", "The Eyrie")
+all_dist = dist1 + dist2
+print(all_dist)
+# END
