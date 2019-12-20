247b5b383fc0b078676d2b24f7695dbcc3c91a7d
maksdk <havryshchenko@gmail.com>
2019-12-20 09:55:15 +0200

    arguments-as-expressions

---
+++ 35.py
@@ -0,0 +1,9 @@
+from hexlet.code_basics import calculate_distance_between_towns
+
+source = 'The Twins'
+destination = 'The Eyrie'
+
+# BEGIN
+template = "{}-{}"
+print(calculate_distance_between_towns(template.format(source, destination)))
+# END
